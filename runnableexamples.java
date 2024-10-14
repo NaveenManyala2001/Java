cb22efaa661cf73918c8de55a259f02c62120b40
NaveenManyala2001 <116255598+NaveenManyala2001@users.noreply.github.com>
2024-10-14 12:56:08 +0530

    Created

---
+++ runnableexamples.java
@@ -0,0 +1,23 @@
+package naveen;
+class Multi1 implements Runnable
+{
+	public void run()
+	{
+		System.out.println("thread is running");
+	}
+	
+}
+
+public class runnableexamples {
+
+	public static void main(String[] args) {
+		Multi1 m1=new Multi1();
+		Thread t1=new Thread(m1);
+		t1.start();
+		
+
+		
+
+	}
+
+}
